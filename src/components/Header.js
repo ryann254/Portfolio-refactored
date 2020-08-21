50a75bdc6e9828cab47eea21357093cd18cb6557
ryann254 <keddelyronjoz@gmail.com>
2020-08-21 14:17:24 +0300

    Done with the header on 1200px view

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,7 @@ import $ from "jquery";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
+import NavItem from "react-bootstrap/NavItem";
 
 import "./Header.css";
 
@@ -49,22 +50,6 @@ function Header({ children }) {
     setDesc(logoDesc);
   }, []);
 
-  // Changing menu color according to the page rendered
-  function menuColorChange() {
-    if (btnLines !== null) {
-      btnLines.forEach((item) => {
-        item.classList.add("add-events");
-      });
-    }
-  }
-  function removeMenuColorChange() {
-    if (btnLines !== null) {
-      btnLines.forEach((item) => {
-        item.classList.remove("add-events");
-      });
-    }
-  }
-
   // Removes the Link color
   function removeLinkColor() {
     Array.from(mainNav).forEach((navLink) => {
@@ -107,14 +92,12 @@ function Header({ children }) {
       headerStyling("about");
     }
   } else if (url.charAt(1) === "w") {
-    removeMenuColorChange();
     removeLinkColor();
     let workLink = document.getElementById("work-link");
     if (workLink !== null) {
       headerStyling("work");
     }
   } else if (url.charAt(1) === "c") {
-    removeMenuColorChange();
     removeLinkColor();
     let contactLink = document.getElementById("contact-link");
     if (contactLink !== null) {
@@ -125,10 +108,8 @@ function Header({ children }) {
     let addEventsLink = document.getElementById("add-events-link");
     if (addEventsLink !== null) {
       headerStyling("add-events");
-      menuColorChange();
     }
   } else {
-    removeMenuColorChange();
     removeLinkColor();
     let homeLink = document.getElementById("home-link");
     if (homeLink !== null) {
@@ -136,171 +117,53 @@ function Header({ children }) {
     }
   }
 
-  //Closes the menu on moving to a different page
-  function handleClose() {
-    setShowMenu(false);
-    menuBtn.classList.remove("close");
-    menu.classList.remove("show");
-    menuNav.classList.remove("show");
-    navItems.forEach((item) => {
-      item.classList.remove("show");
-    });
-  }
-
-  //Open and Closes the menu
-  const toggleMenu = () => {
-    if (!showMenu) {
-      if (menuBtn !== null) {
-        menuBtn.classList.add("close");
-        menu.classList.add("show");
-        menuNav.classList.add("show");
-        navItems.forEach((item) => {
-          item.classList.add("show");
-        });
-        setShowMenu(true);
-      }
-    } else {
-      handleClose();
-    }
-  };
-
-  //Adds hover effect to the logo elements
-  function addHover() {
-    if (!hover) {
-      border.classList.add("hover");
-      desc.classList.add("hover");
-      toggleHover(true);
-    } else {
-      toggleHover(false);
-      border.classList.remove("hover");
-      desc.classList.remove("hover");
-    }
-  }
-
-  //Making the header sticky
-  function stickyHeader() {
-    $(document).ready(function () {
-      $(window).scroll(function () {
-        if ($(window).scrollTop() > 30) {
-          $("#nav-bar").addClass("navbar-fixed");
-        }
-        if ($(window).scrollTop() < 31) {
-          $("#nav-bar").removeClass("navbar-fixed");
-        }
-      });
-    });
-  }
-
   return (
     <Fragment>
-      {stickyHeader()}
-      <div id="bg-img">
-        <header>
-          <Container fluid>
-            <Navbar bg="light" expand="md">
-              <Navbar.Brand href="/" className="brand"></Navbar.Brand>
-              <Navbar.Toggle aria-controls="basic-navbar-nav" />
-              <Navbar.Collapse id="basic-navbar-nav">
-                <Nav></Nav>
-              </Navbar.Collapse>
-            </Navbar>
-          </Container>
-        </header>
-        <div id="nav-bar">
-          <Link to="/" onMouseEnter={addHover} onMouseLeave={addHover}>
-            <div className="logo-container">
-              <div className="logo-border" id="logo-border">
-                <div className="logo">R</div>
-              </div>
-              <div>
-                <div className="logo-name">
-                  Ryan Njoroge
-                  <hr className="line"></hr>
-                  <div className="logo-desc" id="logo-desc">
-                    Web Developer & Ai Enthusiast
+      <header>
+        <Container fluid>
+          <Navbar bg="light" expand="md">
+            <Navbar.Brand href="/" className="brand ml-lg-4">
+              RN
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav id="growth">
+                <NavItem>
+                  <div className="links text-center" id="home-link">
+                    <Link to="/">Home</Link>
+                    <div className="underline active"></div>
                   </div>
-                </div>
-              </div>
-            </div>
-          </Link>
-          <div className="navigation-links">
-            <div className="links" id="home-link">
-              <Link to="/">Home</Link>
-              <div className="underline active"></div>
-            </div>
-            <div className="links" id="about-link">
-              <Link to="/about"> About Me</Link>
-              <div className="underline"></div>
-            </div>
-            <div className="links" id="work-link">
-              <Link to="/work">My Work</Link>
-              <div className="underline"></div>
-            </div>
-            <div className="links" id="add-events-link">
-              <Link to="/add-events">Add Events</Link>
-              <div className="underline"></div>
-            </div>
-            <div className="links" id="contact-link">
-              <Link to="/contact">Contact</Link>
-              <div className="underline"></div>
-            </div>
-          </div>
-
-          <div className="menu-btn" onClick={() => toggleMenu()}>
-            <div className="btn-line" id="line1"></div>
-            <div className="btn-line" id="line2"></div>
-            <div className="btn-line" id="line3"></div>
-          </div>
-        </div>
-
-        <nav className="menu">
-          <ul className="menu-nav">
-            <div className="menu-nav-position"></div>
-            <li id="home" className="nav-item">
-              <Link to="/" className="nav-link" onClick={() => handleClose()}>
-                Home
-              </Link>
-            </li>
-            <li id="about" className="nav-item">
-              <Link
-                to="/about"
-                className="nav-link"
-                onClick={() => handleClose()}
-              >
-                About Me
-              </Link>
-            </li>
-            <li id="work" className="nav-item">
-              <Link
-                to="/work"
-                className="nav-link"
-                onClick={() => handleClose()}
-              >
-                My Work
-              </Link>
-            </li>
-            <li id="add-events" className="nav-item">
-              <Link
-                to="/add-events"
-                className="nav-link"
-                onClick={() => handleClose()}
-              >
-                Add Events
-              </Link>
-            </li>
-            <li id="contact" className="nav-item">
-              <Link
-                to="/contact"
-                className="nav-link"
-                onClick={() => handleClose()}
-              >
-                How To Reach Me
-              </Link>
-            </li>
-          </ul>
-        </nav>
-        {children}
-      </div>
+                </NavItem>
+                <NavItem>
+                  <div className="links text-center" id="about-link">
+                    <Link to="/about"> About Me</Link>
+                    <div className="underline"></div>
+                  </div>
+                </NavItem>
+                <NavItem>
+                  <div className="links text-center" id="work-link">
+                    <Link to="/work">My Work</Link>
+                    <div className="underline"></div>
+                  </div>
+                </NavItem>
+                <NavItem>
+                  <div className="links text-center" id="add-events-link">
+                    <Link to="/add-events">Add Events</Link>
+                    <div className="underline"></div>
+                  </div>
+                </NavItem>
+                <NavItem>
+                  <div className="links text-center" id="contact-link">
+                    <Link to="/contact">Contact</Link>
+                    <div className="underline"></div>
+                  </div>
+                </NavItem>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+        </Container>
+      </header>
+      {children}
     </Fragment>
   );
 }
