c25f0b8d0cc802384e318285e83095e1babf925d
ryann254 <keddelyronjoz@gmail.com>
2020-09-23 17:46:44 +0300

    Removed previous loader

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, Fragment } from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter } from "react-router-dom";
 import { useDispatch } from "react-redux";
 
