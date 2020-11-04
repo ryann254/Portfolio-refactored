471bcbc0795d1d9a629ed4d1fb3ceb2013ea6ee1
Ryan Njoroge <keddelyronjoz@gmail.com>
2020-11-04 13:15:01 +0300

    Fixed data error issue

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import React, { useEffect, Fragment } from "react";
-import Link from "react-router-dom/Link";
+import {Link} from "react-router-dom";
 
 import "./Header.css";
 import Container from "react-bootstrap/Container";
