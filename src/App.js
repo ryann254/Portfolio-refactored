cad3e7aa6855e80c08999a7af2693afdc1fb48bb
ryann254 <keddelyronjoz@gmail.com>
2022-10-11 09:49:10 +0300

    feat: add graphql to the project

--- src/App.js
+++ src/App.js
@@ -1,52 +1,59 @@
-import React, { useEffect } from "react";
-import {BrowserRouter} from "react-router-dom";
-import {useDispatch} from "react-redux";
+import React, { useEffect } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { ApolloClient } from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 
 //Own Components
-import "./App.css";
-import Api from "./services/network";
-import Routes from "./Routes";
-import PageLoader from "./page-loader/PageLoader";
-import { getAllComments, getErrors } from "./redux/action-creator";
-import isEmpty from "./helpers/isEmpty";
+import './App.css';
+import Api from './services/network';
+import Routes from './Routes';
+import PageLoader from './page-loader/PageLoader';
+import { getAllComments, getErrors } from './redux/action-creator';
+import isEmpty from './helpers/isEmpty';
 
 function App() {
-  const dispatch = useDispatch();
-  // Making the api call
-  async function getComments() {
-    const api = new Api();
-    try {
-      const res = await api.auth().getCommentsCall();
-      if (res.status === 200) {
-        const { date } = res.data;
-        data.map((element) => {
-          element.radio = "comments";
-        });
+	const client = new ApolloClient({
+		uri: 'https://api-eu-west-2.hygraph.com/v2/cl93rprfe474l01tcdu78gvjk/master',
+	});
+	const dispatch = useDispatch();
+	// Making the api call
+	async function getComments() {
+		const api = new Api();
+		try {
+			const res = await api.auth().getCommentsCall();
+			if (res.status === 200) {
+				const { data } = res.data;
+				data.map((element) => {
+					element.radio = 'comments';
+				});
 
-        dispatch(getAllComments(data));
-      }
-    } catch (error) {
-      if (isEmpty(error) !== true) {
-        console.log("Isempty is not working")
-        const dbErrors = error.response.data.error;
-        dispatch(getErrors(dbErrors));
-      }
-    }
-  }
+				dispatch(getAllComments(data));
+			}
+		} catch (error) {
+			if (isEmpty(error) !== true) {
+				console.log('Isempty is not working');
+				const dbErrors = error.response.data.error;
+				dispatch(getErrors(dbErrors));
+			}
+		}
+	}
 
-  useEffect(() => {
-    getComments();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-  return (
-    <React.Suspense fallback={<PageLoader />}>
-      <BrowserRouter>
-        <div className="App">
-          <Routes />
-        </div>
-      </BrowserRouter>
-    </React.Suspense>
-  );
+	useEffect(() => {
+		getComments();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
+	return (
+		<React.Suspense fallback={<PageLoader />}>
+			<ApolloProvider client={client}>
+				<BrowserRouter>
+					<div className='App'>
+						<Routes />
+					</div>
+				</BrowserRouter>
+			</ApolloProvider>
+		</React.Suspense>
+	);
 }
 
 export default App;
