4475ede4e31d169a730fe039adb091c190581bc1
ryann254 <keddelyronjoz@gmail.com>
2022-12-06 13:07:24 +0300

    feat: downgrade graphql and remove command to set nodejs version

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,12 +1,10 @@
-import applyMiddleware from "redux/src/applyMiddleware";
-import createStore from "redux/src/createStore";
-import combineReducers from "redux/src/combineReducers";
-import CreateSagaMiddleware from "redux-saga";
-import { composeWithDevTools } from "redux-devtools-extension";
+import { combineReducers, createStore, applyMiddleware } from 'redux';
+import CreateSagaMiddleware from 'redux-saga';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-import AboutReducer from "../reducer/AboutReducer";
-import rootSaga from "../middleware";
-import WorkReducer from "../reducer/WorkReducer";
+import AboutReducer from '../reducer/AboutReducer';
+import rootSaga from '../middleware';
+import WorkReducer from '../reducer/WorkReducer';
 
 const sagaMiddleware = CreateSagaMiddleware();
 const middleware = composeWithDevTools(applyMiddleware(sagaMiddleware));
@@ -14,7 +12,7 @@ const middleware = composeWithDevTools(applyMiddleware(sagaMiddleware));
 const store = createStore(
   combineReducers({
     about: AboutReducer,
-    work: WorkReducer 
+    work: WorkReducer,
   }),
   middleware
 );
