93bf24da7698d7dfa9f3fe2b4cb8823384b7a0d6
Ryan Njoroge <45309245+ryann254@users.noreply.github.com>
2023-02-20 13:39:14 +0300

    fix: rearrange the jobs to show newest first (#29)

--- src/components/Work.js
+++ src/components/Work.js
@@ -11,127 +11,127 @@ import JOBS_QUERY from '../graphql/jobs';
 import { useEffect, useState } from 'react';
 
 function Work() {
-	// Projects without the problem subtitle.
-	const withoutChallenge = [
-		'React Messenger',
-		'Wedding Planner Template',
-		'Bakery Template',
-		'Fancy Form',
-	];
+  // Projects without the problem subtitle.
+  const withoutChallenge = [
+    'React Messenger',
+    'Wedding Planner Template',
+    'Bakery Template',
+    'Fancy Form',
+  ];
 
-	const [portfolioJobs, setPortfolioJobs] = useState([]);
+  const [portfolioJobs, setPortfolioJobs] = useState([]);
 
-	const { loading, error, data } = useQuery(JOBS_QUERY);
-	useEffect(() => {
-		if (!loading && !error) {
-			setPortfolioJobs(data.portfolioJobs);
-		}
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [loading]);
+  const { loading, error, data } = useQuery(JOBS_QUERY);
+  useEffect(() => {
+    if (!loading && !error) {
+      setPortfolioJobs(data.portfolioJobs);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [loading]);
 
-	if (loading) return <h1>Loading...</h1>;
-	if (error) return <h1>Error!</h1>;
+  if (loading) return <h1>Loading...</h1>;
+  if (error) return <h1>Error!</h1>;
 
-	return (
-		<Fragment>
-			<Container>
-				<div id='work'>
-					<h1 className='lg-heading mb-2'>
-						<span className='text-secondary'>My</span> Work
-					</h1>
-					<h2 className='sm-heading'>A few of my projects....</h2>
-					<div className='projects'>
-						{portfolioJobs.length ? (
-							portfolioJobs.map((job, index) => (
-								<Row className='mb-5' key={job.id}>
-									<div className='d-flex-column d-md-flex'>
-										<Col>
-											<div className='item mb-4'>
-												<a href={job.netlifyLink} target='_blank'>
-													<img
-														src={job.jobImage.url}
-														className={
-															job.title === 'Salon App'
-																? 'salon covid-19'
-																: 'covid-19'
-														}
-														alt='Project'
-													/>
-												</a>
-												<div className='job-buttons'>
-													<a
-														href={job.netlifyLink}
-														target='_blank'
-														className='btn btn-light'
-													>
-														<i className='fas fa-eye pr-1'></i> Project
-													</a>
+  return (
+    <Fragment>
+      <Container>
+        <div id='work'>
+          <h1 className='lg-heading mb-2'>
+            <span className='text-secondary'>My</span> Work
+          </h1>
+          <h2 className='sm-heading'>A few of my projects....</h2>
+          <div className='projects'>
+            {portfolioJobs.length ? (
+              portfolioJobs.map((job, index) => (
+                <Row className='mb-5' key={job.id}>
+                  <div className='d-flex-column d-md-flex'>
+                    <Col>
+                      <div className='item mb-4'>
+                        <a href={job.netlifyLink} target='_blank'>
+                          <img
+                            src={job.jobImage.url}
+                            className={
+                              job.title === 'Salon App'
+                                ? 'salon covid-19'
+                                : 'covid-19'
+                            }
+                            alt='Project'
+                          />
+                        </a>
+                        <div className='job-buttons'>
+                          <a
+                            href={job.netlifyLink}
+                            target='_blank'
+                            className='btn btn-light'
+                          >
+                            <i className='fas fa-eye pr-1'></i> Project
+                          </a>
 
-													<a
-														href={job.githubLink}
-														target='_blank'
-														className='btn btn-dark'
-													>
-														<i className='fab fa-github pr-1'></i> Github
-													</a>
-												</div>
-											</div>
-										</Col>
-										<Col>
-											<div className='job-content'>
-												<div>
-													<h3>{job.title}</h3>
-													{!withoutChallenge.includes(job.title) ? (
-														<h3>Challenge:</h3>
-													) : null}
-													{job.title.includes('Homefurniture') ? (
-														<h3>Small scale e-commerce site:</h3>
-													) : null}
-												</div>
-												<div>
-													<p>{job.description}</p>
-												</div>
-												{job.solution !== null ? (
-													<div>
-														<h3>Solution:</h3>
-														<p>{job.solution}</p>
-													</div>
-												) : null}
-												{job.title.includes('React') ||
-												job.title.includes('Daily') ? (
-													<div>
-														<h3>How to use</h3> <p>{job.usage}</p>
-													</div>
-												) : null}
-												<div>
-													<>
-														{job.noteableFeatures !== null ? (
-															<>
-																<h3>Noteable Features:</h3>
-																<p>{job.noteableFeatures}</p>
-															</>
-														) : (
-															<></>
-														)}
+                          <a
+                            href={job.githubLink}
+                            target='_blank'
+                            className='btn btn-dark'
+                          >
+                            <i className='fab fa-github pr-1'></i> Github
+                          </a>
+                        </div>
+                      </div>
+                    </Col>
+                    <Col>
+                      <div className='job-content'>
+                        <div>
+                          <h3>{job.title}</h3>
+                          {!withoutChallenge.includes(job.title) ? (
+                            <h3>Challenge:</h3>
+                          ) : null}
+                          {job.title.includes('Homefurniture') ? (
+                            <h3>Small scale e-commerce site:</h3>
+                          ) : null}
+                        </div>
+                        <div>
+                          <p>{job.description}</p>
+                        </div>
+                        {job.solution !== null ? (
+                          <div>
+                            <h3>Solution:</h3>
+                            <p>{job.solution}</p>
+                          </div>
+                        ) : null}
+                        {job.title.includes('React') ||
+                        job.title.includes('Daily') ? (
+                          <div>
+                            <h3>How to use</h3> <p>{job.usage}</p>
+                          </div>
+                        ) : null}
+                        <div>
+                          <>
+                            {job.noteableFeatures !== null ? (
+                              <>
+                                <h3>Noteable Features:</h3>
+                                <p>{job.noteableFeatures}</p>
+                              </>
+                            ) : (
+                              <></>
+                            )}
 
-														<h3>Languages:</h3>
-														<p>{job.languages}</p>
-													</>
-												</div>
-											</div>
-										</Col>
-									</div>
-								</Row>
-							))
-						) : (
-							<div>Loading...</div>
-						)}
-					</div>
-				</div>
-			</Container>
-			<Footer />
-		</Fragment>
-	);
+                            <h3>Languages:</h3>
+                            <p>{job.languages}</p>
+                          </>
+                        </div>
+                      </div>
+                    </Col>
+                  </div>
+                </Row>
+              ))
+            ) : (
+              <div>Loading...</div>
+            )}
+          </div>
+        </div>
+      </Container>
+      <Footer />
+    </Fragment>
+  );
 }
 
 export default Work;
--- src/graphql/jobs.js
+++ src/graphql/jobs.js
@@ -1,23 +1,23 @@
 import { gql } from '@apollo/client';
 
 const JOBS_QUERY = gql`
-	query {
-		portfolioJobs {
-			id
-			netlifyLink
-			githubLink
-			title
-			description
-			solution
-			languages
-			usage
-			noteableFeatures
-			jobImage {
-				id
-				url
-			}
-		}
-	}
+  query {
+    portfolioJobs(orderBy: createdAt_DESC) {
+      id
+      netlifyLink
+      githubLink
+      title
+      description
+      solution
+      languages
+      usage
+      noteableFeatures
+      jobImage {
+        id
+        url
+      }
+    }
+  }
 `;
 
 export default JOBS_QUERY;
