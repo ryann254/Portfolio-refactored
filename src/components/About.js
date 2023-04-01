4bd5e6f747f48dc9dcfd079a35beb6771e2a37ca
ryann254 <keddelyronjoz@gmail.com>
2023-04-01 15:19:46 +0300

    feat: removed comment section, add-events and login pages

--- src/components/About.js
+++ src/components/About.js
@@ -13,310 +13,253 @@ import ProfilePic from '../../public/images/Profile-replacement1-min.webp';
 import './About.css';
 import Footer from './Footer';
 import {
-	addBioContent,
-	addBioIntro,
-	changeScrollStatus,
-	deleteComment,
-	getErrors,
+  addBioContent,
+  addBioIntro,
+  changeScrollStatus,
+  deleteComment,
+  getErrors,
 } from '../redux/action-creator';
 import { useQuery } from '@apollo/client';
 import INTRO_QUERY from '../graphql/intro';
 import ABOUT_QUERY from '../graphql/about';
 
 function About() {
-	const { commentsSection, scrollToElement, bioIntro, bioContent } =
-		useSelector((state) => state.about);
+  const { commentsSection, scrollToElement, bioIntro, bioContent } =
+    useSelector((state) => state.about);
 
-	const dispatch = useDispatch();
-	const [buttonState, setState] = useState(false);
-	const [workandEvents, setWorkandEvents] = useState({ work: [], events: [] });
+  const dispatch = useDispatch();
+  const [buttonState, setState] = useState(false);
+  const [workandEvents, setWorkandEvents] = useState({ work: [], events: [] });
 
-	function doneResizing(id, offsetTop) {
-		if (window.Modernizr.mq('screen and (min-width:768px)')) {
-			if (offsetTop === '') {
-				let offsetTop = 130;
-				scrollFunction(id, offsetTop);
-			} else {
-				scrollFunction(id, offsetTop);
-			}
-		} else if (window.Modernizr.mq('screen and (max-width: 767px)')) {
-			if (offsetTop === '') {
-				let offsetTop = 500;
-				scrollFunction(id, offsetTop);
-			} else {
-				scrollFunction(id, offsetTop);
-			}
-		}
-	}
+  function doneResizing(id, offsetTop) {
+    if (window.Modernizr.mq('screen and (min-width:768px)')) {
+      if (offsetTop === '') {
+        let offsetTop = 130;
+        scrollFunction(id, offsetTop);
+      } else {
+        scrollFunction(id, offsetTop);
+      }
+    } else if (window.Modernizr.mq('screen and (max-width: 767px)')) {
+      if (offsetTop === '') {
+        let offsetTop = 500;
+        scrollFunction(id, offsetTop);
+      } else {
+        scrollFunction(id, offsetTop);
+      }
+    }
+  }
 
-	function scrollFunction(id, addTop) {
-		let offsetTop = document.getElementById(`${id}`).offsetTop;
-		console.log(addTop);
-		window.scrollTo({
-			top: offsetTop + addTop,
-			behavior: 'smooth',
-		});
-		dispatch(changeScrollStatus());
-	}
+  function scrollFunction(id, addTop) {
+    let offsetTop = document.getElementById(`${id}`).offsetTop;
+    console.log(addTop);
+    window.scrollTo({
+      top: offsetTop + addTop,
+      behavior: 'smooth',
+    });
+    dispatch(changeScrollStatus());
+  }
 
-	const handleClick = () => {
-		const jobElement = document.getElementsByClassName('comment');
+  const handleClick = () => {
+    const jobElement = document.getElementsByClassName('comment');
 
-		let element = 0;
-		for (element; element < jobElement.length; element++) {
-			jobElement[element].classList.add('delete');
-		}
-		setState((buttonState) => !buttonState);
-	};
+    let element = 0;
+    for (element; element < jobElement.length; element++) {
+      jobElement[element].classList.add('delete');
+    }
+    setState((buttonState) => !buttonState);
+  };
 
-	async function deleteComments(id) {
-		const api = new Api();
-		try {
-			const res = await api.auth().deleteCommentCall(id);
-			dispatch(deleteComment(id));
-		} catch (error) {
-			const dbErrors = error.response.data.error;
-			dispatch(getErrors(dbErrors));
-		}
-	}
+  async function deleteComments(id) {
+    const api = new Api();
+    try {
+      const res = await api.auth().deleteCommentCall(id);
+      dispatch(deleteComment(id));
+    } catch (error) {
+      const dbErrors = error.response.data.error;
+      dispatch(getErrors(dbErrors));
+    }
+  }
 
-	useEffect(() => {
-		if (scrollToElement.state === true) {
-			if (
-				scrollToElement.section !== '' &&
-				scrollToElement.section === 'work'
-			) {
-				doneResizing('work-section');
-			} else if (
-				scrollToElement.section !== '' &&
-				scrollToElement.section === 'events'
-			) {
-				let offsetTop = 1300;
-				doneResizing('events-section', offsetTop);
-			} else {
-				let offsetTop = -130;
-				doneResizing('comments-section', offsetTop);
-			}
-		}
-	}, [scrollToElement]);
+  useEffect(() => {
+    if (scrollToElement.state === true) {
+      if (
+        scrollToElement.section !== '' &&
+        scrollToElement.section === 'work'
+      ) {
+        doneResizing('work-section');
+      } else if (
+        scrollToElement.section !== '' &&
+        scrollToElement.section === 'events'
+      ) {
+        let offsetTop = 1300;
+        doneResizing('events-section', offsetTop);
+      } else {
+        let offsetTop = -130;
+        doneResizing('comments-section', offsetTop);
+      }
+    }
+  }, [scrollToElement]);
 
-	const { loading, error, data } = useQuery(INTRO_QUERY);
-	const about = useQuery(ABOUT_QUERY);
+  const { loading, error, data } = useQuery(INTRO_QUERY);
+  const about = useQuery(ABOUT_QUERY);
 
-	useEffect(() => {
-		if (!bioContent.length && !bioIntro) {
-			if (!loading && !error) {
-				dispatch(addBioIntro(data.bioIntros[0].intro));
-				dispatch(addBioContent(data.bioBodies));
-			}
-		}
+  useEffect(() => {
+    if (!bioContent.length && !bioIntro) {
+      if (!loading && !error) {
+        dispatch(addBioIntro(data.bioIntros[0].intro));
+        dispatch(addBioContent(data.bioBodies));
+      }
+    }
 
-		if (!about.loading && !about.error) {
-			setWorkandEvents({
-				work: about.data.workAndEducations,
-				events: about.data.eventsAndAchievements,
-			});
-		}
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [loading, about.loading]);
+    if (!about.loading && !about.error) {
+      setWorkandEvents({
+        work: about.data.workAndEducations,
+        events: about.data.eventsAndAchievements,
+      });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [loading, about.loading]);
 
-	if (loading) return <h1>Loading...</h1>;
-	if (error) return <h1>Error!</h1>;
-	return (
-		<Fragment>
-			<Container>
-				<div id='about'>
-					<h1 className='lg-heading mb-2'>
-						<span className='text-secondary'>About</span> Me
-					</h1>
-					<h2 className='sm-heading'>Let me mention just a few...</h2>
-					<div className='about-info'>
-						<Row>
-							<div className='image-bio d-flex-column d-md-flex'>
-								<Col md={6} className='profile-pic my-auto'>
-									<Bounce right>
-										<div className='d-flex justify-content-center'>
-											<img
-												src={ProfilePic}
-												width={280}
-												height={280}
-												alt='Bio'
-												className='bio-image'
-											/>
-										</div>
-									</Bounce>
-								</Col>
-								<Col md={6} className='mx-auto mt-5'>
-									{bioIntro && bioContent.length ? (
-										<div className='bio'>
-											<h3 className='text-secondary'>BIO</h3>
-											<p className='show'>
-												{bioIntro}
-												<br />
-												<span className='text-secondary-paragraph'>
-													<strong>{bioContent[0].subtitle}</strong>
-												</span>
-												{bioContent[0].subtitleBody}
-												<br />
-												<span className='text-secondary-paragraph'>
-													<strong>{bioContent[1].subtitle}</strong>
-												</span>
-												{bioContent[1].subtitleBody} <br />
-												<span className='text-secondary-paragraph'>
-													<strong>{bioContent[2].subtitle}</strong>
-												</span>
-												{bioContent[2].subtitleBody} <br />
-											</p>
-										</div>
-									) : null}
-								</Col>
-							</div>
-						</Row>
-						<h2 className='sm-heading work'>Work and Education</h2>
-						<Zoom>
-							<div className='work-education d-flex mb-5' id='work-section'>
-								<Row>
-									{workandEvents.work.length
-										? workandEvents.work.map((item, index) => (
-												<Col key={index} md={6} lg={4} className='mb-4'>
-													<div className='job' key={index}>
-														<div className='d-flex'>
-															<div
-																className='identify'
-																style={{
-																	background: randomcolor({
-																		luminosity: 'dark',
-																		format: 'rgba',
-																		alpha: 0.7,
-																	}),
-																}}
-															>
-																{item.title !== ''
-																	? item.title.slice(0, 1).toUpperCase()
-																	: null}
-															</div>
-															<div>
-																<h3 className='mt-2 work-title'>
-																	{item.title}
-																</h3>
-																<h4>{item.subtitle}</h4>
-															</div>
-														</div>
+  if (loading) return <h1>Loading...</h1>;
+  if (error) return <h1>Error!</h1>;
+  return (
+    <Fragment>
+      <Container>
+        <div id='about'>
+          <h1 className='lg-heading mb-2'>
+            <span className='text-secondary'>About</span> Me
+          </h1>
+          <h2 className='sm-heading'>Let me mention just a few...</h2>
+          <div className='about-info'>
+            <Row>
+              <div className='image-bio d-flex-column d-md-flex'>
+                <Col md={6} className='profile-pic my-auto'>
+                  <Bounce right>
+                    <div className='d-flex justify-content-center'>
+                      <img
+                        src={ProfilePic}
+                        width={280}
+                        height={280}
+                        alt='Bio'
+                        className='bio-image'
+                      />
+                    </div>
+                  </Bounce>
+                </Col>
+                <Col md={6} className='mx-auto mt-5'>
+                  {bioIntro && bioContent.length ? (
+                    <div className='bio'>
+                      <h3 className='text-secondary'>BIO</h3>
+                      <p className='show'>
+                        {bioIntro}
+                        <br />
+                        <span className='text-secondary-paragraph'>
+                          <strong>{bioContent[0].subtitle}</strong>
+                        </span>
+                        {bioContent[0].subtitleBody}
+                        <br />
+                        <span className='text-secondary-paragraph'>
+                          <strong>{bioContent[1].subtitle}</strong>
+                        </span>
+                        {bioContent[1].subtitleBody} <br />
+                        <span className='text-secondary-paragraph'>
+                          <strong>{bioContent[2].subtitle}</strong>
+                        </span>
+                        {bioContent[2].subtitleBody} <br />
+                      </p>
+                    </div>
+                  ) : null}
+                </Col>
+              </div>
+            </Row>
+            <h2 className='sm-heading work'>Work and Education</h2>
+            <Zoom>
+              <div className='work-education d-flex mb-5' id='work-section'>
+                <Row>
+                  {workandEvents.work.length
+                    ? workandEvents.work.map((item, index) => (
+                        <Col key={index} md={6} lg={4} className='mb-4'>
+                          <div className='job' key={index}>
+                            <div className='d-flex'>
+                              <div
+                                className='identify'
+                                style={{
+                                  background: randomcolor({
+                                    luminosity: 'dark',
+                                    format: 'rgba',
+                                    alpha: 0.7,
+                                  }),
+                                }}
+                              >
+                                {item.title !== ''
+                                  ? item.title.slice(0, 1).toUpperCase()
+                                  : null}
+                              </div>
+                              <div>
+                                <h3 className='mt-2 work-title'>
+                                  {item.title}
+                                </h3>
+                                <h4>{item.subtitle}</h4>
+                              </div>
+                            </div>
 
-														<p>{item.body}</p>
-													</div>
-												</Col>
-										  ))
-										: null}
-								</Row>
-							</div>
-						</Zoom>
-						<div className='headings'>
-							<h2 className='sm-heading work'>Events and Achievements</h2>
-						</div>
-						<Zoom>
-							<div
-								className='work-education d-flex mx-auto'
-								id='events-section'
-							>
-								<Row>
-									{workandEvents.events.length
-										? workandEvents.events.map((item, index) => (
-												<Col key={index} md={6} lg={4} className='mb-4'>
-													<div className='job' key={index}>
-														<div className='d-flex'>
-															<div
-																className='identify'
-																style={{
-																	background: randomcolor({
-																		luminosity: 'dark',
-																		format: 'rgba',
-																		alpha: 0.7,
-																	}),
-																}}
-															>
-																{item.title !== ''
-																	? item.title.slice(0, 1).toUpperCase()
-																	: null}
-															</div>
-															<div>
-																<h3 className='mt-2 work-title'>
-																	{item.title}
-																</h3>
-																<h4>{item.subtitle}</h4>
-															</div>
-														</div>
-														<p>{item.body}</p>
-													</div>
-												</Col>
-										  ))
-										: console.log('Loading')}
-								</Row>
-							</div>
-						</Zoom>
-						<div className='headings'>
-							<h2 className='sm-heading work'>Comments Section</h2>
-						</div>
-						<Zoom>
-							<div
-								className='work-education d-flex mx-auto'
-								id='comments-section'
-							>
-								<Row>
-									{commentsSection !== ''
-										? commentsSection.map((item, index) => (
-												<Col key={index} className='mb-4'>
-													<div
-														className='job comment'
-														key={index}
-														onClick={handleClick}
-													>
-														<div className='d-flex'>
-															<div
-																className='identify'
-																style={{
-																	background: randomcolor({
-																		luminosity: 'dark',
-																		format: 'rgba',
-																		alpha: 0.7,
-																	}),
-																}}
-															>
-																{item.title !== ''
-																	? item.title.slice(0, 1).toUpperCase()
-																	: null}
-															</div>
-															<div>
-																<h3 className='mt-2 work-title'>
-																	{item.title}
-																</h3>
-																<h4>{item.subtitle}</h4>
-															</div>
-														</div>
-														<p>{item.content}</p>
-													</div>
-													{buttonState ? (
-														<Button
-															variant='outline-danger'
-															id='delete-btn'
-															size='sm'
-															onClick={() => deleteComments(item._id)}
-														>
-															Delete
-														</Button>
-													) : null}
-												</Col>
-										  ))
-										: console.log('Loading')}
-								</Row>
-							</div>
-						</Zoom>
-					</div>
-				</div>
-			</Container>
-			<Footer />
-		</Fragment>
-	);
+                            <p>{item.body}</p>
+                          </div>
+                        </Col>
+                      ))
+                    : null}
+                </Row>
+              </div>
+            </Zoom>
+            <div className='headings'>
+              <h2 className='sm-heading work'>Events and Achievements</h2>
+            </div>
+            <Zoom>
+              <div
+                className='work-education d-flex mx-auto'
+                id='events-section'
+              >
+                <Row>
+                  {workandEvents.events.length
+                    ? workandEvents.events.map((item, index) => (
+                        <Col key={index} md={6} lg={4} className='mb-4'>
+                          <div className='job' key={index}>
+                            <div className='d-flex'>
+                              <div
+                                className='identify'
+                                style={{
+                                  background: randomcolor({
+                                    luminosity: 'dark',
+                                    format: 'rgba',
+                                    alpha: 0.7,
+                                  }),
+                                }}
+                              >
+                                {item.title !== ''
+                                  ? item.title.slice(0, 1).toUpperCase()
+                                  : null}
+                              </div>
+                              <div>
+                                <h3 className='mt-2 work-title'>
+                                  {item.title}
+                                </h3>
+                                <h4>{item.subtitle}</h4>
+                              </div>
+                            </div>
+                            <p>{item.body}</p>
+                          </div>
+                        </Col>
+                      ))
+                    : console.log('Loading')}
+                </Row>
+              </div>
+            </Zoom>
+          </div>
+        </div>
+      </Container>
+      <Footer />
+    </Fragment>
+  );
 }
 
 export default About;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,42 +1,39 @@
-import React, { Fragment, useState, useEffect } from "react";
-import {Link}  from "react-router-dom";
-import { useRouteMatch } from "react-router-dom";
-import Container from "react-bootstrap/Container";
-import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
-import NavItem from "react-bootstrap/NavItem";
-import Button from "react-bootstrap/Button";
-import { useOktaAuth } from "@okta/okta-react";
+import React, { Fragment, useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { useRouteMatch } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import NavItem from 'react-bootstrap/NavItem';
 
-import "./Header.css";
+import './Header.css';
 
 function Header({ children }) {
   const { url } = useRouteMatch();
-  const { authState, authService } = useOktaAuth();
 
-  const [nav, setNav] = useState("");
+  const [nav, setNav] = useState('');
   const [showMenu, setShowMenu] = useState(false);
   const [menuBtn, setMenuBtn] = useState(null);
   const [btnLines, setBtnLines] = useState(null);
   const [menuNav, setMenuNav] = useState(null);
   const [navItems, setNavItem] = useState(null);
   const [menu, setMenu] = useState(null);
-  const [mainNav, setMainNav] = useState("");
-  const [border, setBorder] = useState("");
-  const [desc, setDesc] = useState("");
+  const [mainNav, setMainNav] = useState('');
+  const [border, setBorder] = useState('');
+  const [desc, setDesc] = useState('');
 
   //On component mount the following should be fetched and set to state
   useEffect(() => {
-    let menuBtn = document.querySelector(".menu-btn");
-    let btn1 = document.getElementById("line1");
-    let btn2 = document.getElementById("line2");
-    let btn3 = document.getElementById("line3");
-    let menu = document.querySelector(".menu");
-    let menuNav = document.querySelector(".menu-nav");
-    let navItems = document.querySelectorAll(".nav-item");
-    let navLinks = document.querySelectorAll(".links");
-    let logoBorder = document.getElementById("logo-border");
-    let logoDesc = document.getElementById("logo-desc");
+    let menuBtn = document.querySelector('.menu-btn');
+    let btn1 = document.getElementById('line1');
+    let btn2 = document.getElementById('line2');
+    let btn3 = document.getElementById('line3');
+    let menu = document.querySelector('.menu');
+    let menuNav = document.querySelector('.menu-nav');
+    let navItems = document.querySelectorAll('.nav-item');
+    let navLinks = document.querySelectorAll('.links');
+    let logoBorder = document.getElementById('logo-border');
+    let logoDesc = document.getElementById('logo-desc');
     const btnArray = [];
     btnArray.push(btn1, btn2, btn3);
     setBtnLines(btnArray);
@@ -54,72 +51,68 @@ function Header({ children }) {
   function removeLinkColor() {
     Array.from(mainNav).forEach((navLink) => {
       if (navLink !== undefined) {
-        navLink.children[0].classList.remove("link-color");
-        navLink.children[1].classList.remove("active");
+        navLink.children[0].classList.remove('link-color');
+        navLink.children[1].classList.remove('active');
       }
     });
   }
 
   const addLinkColor = (link) => {
-    link.children[0].classList.add("link-color");
-    link.children[1].classList.add("active");
+    link.children[0].classList.add('link-color');
+    link.children[1].classList.add('active');
   };
 
   const headerStyling = (link) => {
-    if (link === "about") {
-      let aboutLink = document.getElementById("about-link");
+    if (link === 'about') {
+      let aboutLink = document.getElementById('about-link');
       addLinkColor(aboutLink);
-    } else if (link === "work") {
-      let workLink = document.getElementById("work-link");
+    } else if (link === 'work') {
+      let workLink = document.getElementById('work-link');
       addLinkColor(workLink);
-    } else if (link === "contact") {
-      let contactLink = document.getElementById("contact-link");
+    } else if (link === 'contact') {
+      let contactLink = document.getElementById('contact-link');
       addLinkColor(contactLink);
-    } else if (link === "add-events") {
-      let addEventsLink = document.getElementById("add-events-link");
+    } else if (link === 'add-events') {
+      let addEventsLink = document.getElementById('add-events-link');
       addLinkColor(addEventsLink);
     } else {
-      let homeLink = document.getElementById("home-link");
+      let homeLink = document.getElementById('home-link');
       addLinkColor(homeLink);
     }
   };
 
   // Adds the current class to the right link
-  if (url.charAt(2) === "b") {
+  if (url.charAt(2) === 'b') {
     removeLinkColor();
-    let aboutLink = document.getElementById("about-link");
+    let aboutLink = document.getElementById('about-link');
     if (aboutLink !== null) {
-      headerStyling("about");
+      headerStyling('about');
     }
-  } else if (url.charAt(1) === "w") {
+  } else if (url.charAt(1) === 'w') {
     removeLinkColor();
-    let workLink = document.getElementById("work-link");
+    let workLink = document.getElementById('work-link');
     if (workLink !== null) {
-      headerStyling("work");
+      headerStyling('work');
     }
-  } else if (url.charAt(1) === "c") {
+  } else if (url.charAt(1) === 'c') {
     removeLinkColor();
-    let contactLink = document.getElementById("contact-link");
+    let contactLink = document.getElementById('contact-link');
     if (contactLink !== null) {
-      headerStyling("contact");
+      headerStyling('contact');
     }
-  } else if (url.charAt(2) === "d") {
+  } else if (url.charAt(2) === 'd') {
     removeLinkColor();
-    let addEventsLink = document.getElementById("add-events-link");
+    let addEventsLink = document.getElementById('add-events-link');
     if (addEventsLink !== null) {
-      headerStyling("add-events");
+      headerStyling('add-events');
     }
   } else {
     removeLinkColor();
-    let homeLink = document.getElementById("home-link");
+    let homeLink = document.getElementById('home-link');
     if (homeLink !== null) {
       headerStyling(homeLink);
     }
   }
-
-  // Logging In and Out
-  const login = () => authService.login("/");
-  const logout = () => authService.logout("/");
 
   return (
     <Fragment>
@@ -127,59 +120,40 @@ function Header({ children }) {
         <Container fluid>
           <Navbar
             collapseOnSelect
-            expand="md"
-            bg="dark"
-            variant="dark"
-            className="limit"
+            expand='md'
+            bg='dark'
+            variant='dark'
+            className='limit'
           >
-            <Navbar.Brand href="/" className="brand ml-lg-4">
+            <Navbar.Brand href='/' className='brand ml-lg-4'>
               RN
             </Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav id="growth">
+            <Navbar.Toggle aria-controls='basic-navbar-nav' />
+            <Navbar.Collapse id='basic-navbar-nav'>
+              <Nav id='growth'>
                 <NavItem>
-                  <div className="links mb-2 text-center" id="home-link">
-                    <Link to="/">Home</Link>
-                    <div className="underline active"></div>
+                  <div className='links mb-2 text-center' id='home-link'>
+                    <Link to='/'>Home</Link>
+                    <div className='underline active'></div>
                   </div>
                 </NavItem>
                 <NavItem>
-                  <div className="links mb-2 text-center" id="about-link">
-                    <Link to="/about"> About Me</Link>
-                    <div className="underline"></div>
+                  <div className='links mb-2 text-center' id='about-link'>
+                    <Link to='/about'> About Me</Link>
+                    <div className='underline'></div>
                   </div>
                 </NavItem>
                 <NavItem>
-                  <div className="links mb-2 text-center" id="work-link">
-                    <Link to="/work">My Work</Link>
-                    <div className="underline"></div>
+                  <div className='links mb-2 text-center' id='work-link'>
+                    <Link to='/work'>My Work</Link>
+                    <div className='underline'></div>
                   </div>
                 </NavItem>
+
                 <NavItem>
-                  <div className="links mb-2 text-center" id="add-events-link">
-                    <Link to="/add-events">Add Events</Link>
-                    <div className="underline"></div>
-                  </div>
-                </NavItem>
-                <NavItem>
-                  <div className="links mb-2 text-center" id="contact-link">
-                    <Link to="/contact">Contact</Link>
-                    <div className="underline"></div>
-                  </div>
-                </NavItem>
-                <NavItem>
-                  <div className="links mb-2 text-center" id="login-link">
-                    {!authState.isAuthenticated ? (
-                      <Button className="login" onClick={login}>
-                        <a>Login</a>
-                      </Button>
-                    ) : (
-                      <Button className="logout" onClick={logout}>
-                        <a>Logout</a>
-                      </Button>
-                    )}
-                    <div className="underline"></div>
+                  <div className='links mb-2 text-center' id='contact-link'>
+                    <Link to='/contact'>Contact</Link>
+                    <div className='underline'></div>
                   </div>
                 </NavItem>
               </Nav>
